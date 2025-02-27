df557b80297d643c208be537b275cdcd825fca50
marcelo.gomj <marcelo.gomj@gmail.com>
2025-02-27 17:56:07 -0300

    add: tailwindCss v4

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,10 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        sans: ['var(--font-inter)'],
+        jbm: ['var(--font-jetbrains-mono)'],
+      },
     },
   },
   plugins: [],
