bdf37c08cdc5d243af6919bd6598ed1cb03397fb
marcelo-gomj <marcelo.gomj@gmail.com>
2025-03-05 17:56:19 -0300

    add:config for svgr plugin and contentful remote path

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [{
+      hostname: "images.ctfassets.net"
+    }],
+  },
   experimental: {
     turbo: {
       rules: {
