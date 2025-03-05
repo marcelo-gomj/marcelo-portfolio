3cbc1de5b0c1fecded2d4733845912d36ac9b324
marcelo.gomj <marcelo.gomj@gmail.com>
2025-03-05 17:44:44 -0300

    upgrade: tailwindCss v4

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,24 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      ss: { max : "240px"},
+      sm: { max: "320px" },
+      sl: { max: "480px" },
+      ms: { max: "600px" },
+      mm: { max: "768px" },
+      ml: { max: "900px" },
+      ls: { max: "1000px" },
+      lm: { max: "1200px" },
+      lg: { min: "1400px" },
+    },
     extend: {
       colors: {
         background: "var(--background)",
@@ -16,7 +28,9 @@ export default {
         sans: ['var(--font-inter)'],
         jbm: ['var(--font-jetbrains-mono)'],
       },
+      
     },
+    
   },
   plugins: [],
 } satisfies Config;
