88692ab9979d045b6da3da7b4ca34e2ea056b350
marcelo-gomj <marcelo.gomj@gmail.com>
2025-03-10 11:06:19 -0300

    fix:server standart without svgr plugin

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import { NextJsWebpackConfig } from "next/dist/server/config-shared";
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -16,6 +17,30 @@ const nextConfig: NextConfig = {
         },
       },
     },
+  },
+    webpack: (config) => {
+    const fileLoaderRule = config.module.rules.find((rule: any) =>
+      rule.test?.test?.(".svg")
+    );
+
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/,
+      },
+
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
+        use: ["@svgr/webpack"],
+      }
+    );
+
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    return config;
   },
 };
 
